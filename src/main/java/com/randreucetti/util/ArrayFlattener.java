a04cd169912670799ec1ee74faadac5ad874634c
randreucetti <ross.andreucetti@gmail.com>
2015-08-22 11:08:49 +0800

    some javadoc

--- src/main/java/com/randreucetti/util/ArrayFlattener.java
+++ src/main/java/com/randreucetti/util/ArrayFlattener.java
@@ -6,9 +6,21 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 
+ * A utility class used to flatten multidimensional arrays
+ * 
+ * @author ross
+ */
 public class ArrayFlattener {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ArrayFlattener.class);
 
+	/**
+	 * 
+	 * @param array
+	 *            - A multidimensional array to be broken down
+	 * @return A flattened array consisting of the embedded elements of input.
+	 */
 	public static Object[] flatten(Object[] array) {
 		List<Object> returnList = new ArrayList<Object>();
 		flatten(array, returnList);
@@ -16,8 +28,10 @@ public class ArrayFlattener {
 		return returnList.toArray();
 	}
 
+	// recusively break down list
 	private static void flatten(Object[] inputArray, List<Object> returnList) {
 		for (int i = 0; i < inputArray.length; i++) {
+			// do we need to breakdown further
 			if (inputArray[i] instanceof Object[]) {
 				flatten((Object[]) inputArray[i], returnList);
 			} else {
